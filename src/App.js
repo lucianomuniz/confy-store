291e1d6c451f9055dbba922e3dd895cb9b4c5a13
Luciano Muniz <luciano.mo@gmail.com>
2022-06-18 21:35:17 -0300

    added confy-store app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+const App = () => {
+  return <h4>comfy sloth starter</h4>;
+};
+
+export default App;
