4959c2acf94ec4e8781ee80ba487717f32330636
Luciano Muniz <luciano.mo@gmail.com>
2022-06-22 15:52:56 -0300

    added private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,13 +1,12 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth0();
+  const { myUser } = useUserContext();
 
-  if (!user) {
-    <Navigate to='/' />;
+  if (!myUser) {
+    return <Navigate to='/' />;
   }
 
   return children;
